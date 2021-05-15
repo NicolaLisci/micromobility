0d7e3378dabc1b8965c12ae07914de3ae4148143
NicolaLisci <liscinicola@live.it>
2021-05-15 22:34:46 +0200

    Added device location

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -9,61 +10,72 @@ import { environment } from 'src/environments/environment';
 })
 export class HomePage implements OnInit{
   
-  constructor() {}
-  
-  map: mapboxgl.Map;
-  style = 'mapbox://styles/mapbox/streets-v11';
-  lat = 39.03437700146109;
-  lng = 8.999600913492351;
-  
-  geojson = {
-    type: 'FeatureCollection',
-    features: [{
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-77.032, 38.913]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'Washington, D.C.'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-122.414, 37.776]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'San Francisco, California'
-      }
-    }]
-  };
-
-  ngOnInit(): void {
-
-    mapboxgl.accessToken = environment.mapbox.accessToken;
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: this.style,
-      zoom: 15,
-      center: [this.lng, this.lat]
-    });
-
-  //  [39.0355800, 9.0003961]
-    var el = document.createElement('div');
-    el.className = 'marker';
-  
-    // make a marker for each feature and add to the map
-    new mapboxgl.Marker(el)
+  constructor(
+    private geolocation: Geolocation
+    ) {}
+    
+    map: mapboxgl.Map;
+    style = 'mapbox://styles/mapbox/streets-v11';
+    lat = 39.03437700146109;
+    lng = 8.999600913492351;
+    
+    
+    coordinates= {
+      latitude: 0,
+      longitude: 0
+    };
+    
+    ngOnInit(): void {
+      
+      mapboxgl.accessToken = environment.mapbox.accessToken;
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: this.style,
+        zoom: 15,
+        center: [this.lng, this.lat]
+      });
+      
+      //  [39.0355800, 9.0003961]
+      var el = document.createElement('div');
+      el.className = 'marker';
+      
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(el)
       .setLngLat([9.0003961, 39.0355800])
       .addTo(this.map);
-
-    this.map.addControl(new mapboxgl.NavigationControl());
+      
+      this.addDeviceInMap();
+      
+      this.map.addControl(new mapboxgl.NavigationControl());
+    }
+    
+    addDeviceInMap(){
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.coordinates.latitude = resp.coords.latitude;
+        this.coordinates.longitude = resp.coords.longitude
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+      
+      let watch = this.geolocation.watchPosition();
+      watch.subscribe((data:any) => {
+        this.coordinates.latitude = data.coords.latitude;
+        this.coordinates.longitude = data.coords.longitude;
+        
+        console.log(this.coordinates);
+        var el = document.createElement('div');
+        el.className = 'bluetooth';
+        
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+        .setLngLat([this.coordinates.longitude, this.coordinates.latitude])
+        .addTo(this.map);
+      });
+      
+      
+    }
+    
+    
+    
   }
   
-  
-  
-}
