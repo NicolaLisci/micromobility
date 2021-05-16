2796b310d8519f79ac3da168c61b17a47fb424f7
NicolaLisci <liscinicola@live.it>
2021-05-17 00:42:37 +0200

    Added point to ponit map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,7 @@ import { BluetoothService } from '../services/bluetooth.service';
 import { Observable } from 'rxjs';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import * as MapboxDraw from "@mapbox/mapbox-gl-draw";
 
 @Component({
   selector: 'app-home',
@@ -27,6 +28,7 @@ export class HomePage implements OnInit{
     
     
     map: mapboxgl.Map;
+    draw: any;
     style = 'mapbox://styles/mapbox/dark-v10';
     // lat = 39.03437700146109;
     // lng = 8.999600913492351;
@@ -55,6 +57,9 @@ export class HomePage implements OnInit{
       this.addUserLocation();
       this.getUserLocation();
       
+      this.mapDraw();
+      
+      
       this.map.on('load', ()=>{
         this.map.resize();
         this.map.addSource('iso', {
@@ -65,10 +70,14 @@ export class HomePage implements OnInit{
           }
         });
         
+        
+        
+        
         let geocoder = new MapboxGeocoder({
           // Initialize the geocoder
           accessToken: mapboxgl.accessToken, // Set the access token
           mapboxgl: mapboxgl, // Set the mapbox-gl instance
+          localGeocoder: this.coordinatesGeocoder,
           marker: true, // Do not use the default marker style
           placeholder: 'Search', // Placeholder text for the search bar
           proximity: {
@@ -96,6 +105,182 @@ export class HomePage implements OnInit{
           );
           
         });
+      }
+      
+      mapDraw(){
+        this.draw = new MapboxDraw({
+          // Instead of showing all the draw tools, show only the line string and delete tools
+          displayControlsDefault: false,
+          controls: {
+            line_string: true,
+            trash: true
+          },
+          styles: [
+            // Set the line style for the user-input coordinates
+            {
+              'id': 'gl-draw-line',
+              'type': 'line',
+              'filter': [
+                'all',
+                ['==', '$type', 'LineString'],
+                ['!=', 'mode', 'static']
+              ],
+              'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+              },
+              'paint': {
+                'line-color': '#438EE4',
+                'line-dasharray': [0.2, 2],
+                'line-width': 2,
+                'line-opacity': 0.7
+              }
+            },
+            // Style the vertex point halos
+            {
+              'id': 'gl-draw-polygon-and-line-vertex-halo-active',
+              'type': 'circle',
+              'filter': [
+                'all',
+                ['==', 'meta', 'vertex'],
+                ['==', '$type', 'Point'],
+                ['!=', 'mode', 'static']
+              ],
+              'paint': {
+                'circle-radius': 12,
+                'circle-color': '#FFF'
+              }
+            },
+            // Style the vertex points
+            {
+              'id': 'gl-draw-polygon-and-line-vertex-active',
+              'type': 'circle',
+              'filter': [
+                'all',
+                ['==', 'meta', 'vertex'],
+                ['==', '$type', 'Point'],
+                ['!=', 'mode', 'static']
+              ],
+              'paint': {
+                'circle-radius': 8,
+                'circle-color': '#438EE4'
+              }
+            }
+          ]
+        });
+        
+        // Add the draw tool to the map
+        this.map.addControl(this.draw);
+        
+        
+        
+        // Add create, update, or delete actions
+        this.map.on('draw.create',()=> this.updateRoute());
+        this.map.on('draw.update',() => this.updateRoute());
+        // this.map.on('draw.delete', removeRoute);
+        
+      }
+      
+      updateRoute() {
+        // removeRoute(); // Overwrite any existing layers
+        console.log(this.draw);
+        // Get the coordinates
+        let data:any = this.draw.getAll();
+        let lastFeature = data.features.length - 1;
+        let coords = data.features[lastFeature].geometry.coordinates;
+        // Format the coordinates
+        let newCoords = coords.join(';');
+        // Set the radius for each coordinate pair to 25 meters
+        let radius = [];
+        coords.forEach((element) => {
+          radius.push(25);
+        });
+        
+        this.getMatch(newCoords, radius);
+      }
+      
+      // Make a Map Matching request
+      getMatch(coordinates, radius) {
+        // Separate the radiuses with semicolons
+        let radiuses = radius.join(';');
+        // Create the query
+        this.mapService.getMapDraw(coordinates, radiuses).subscribe((res:any)=>{
+          var coords = res.matchings[0].geometry;
+          // Draw the route on the map
+          console.log(coords);
+          this.addRoute(coords);
+          // this.getInstructions(res.matchings[0]);
+        });
+        
+        // $.ajax({
+        //   method: 'GET',
+        //   url: query
+        // }).done(function (data) {
+        //   let coords = data.matchings[0].geometry;
+        // Draw the route on the map
+        // addRoute(coords);
+        // getInstructions(data.matchings[0]);
+        // });
+      }
+      
+      addRoute(coords) {
+        // If a route is already loaded, remove it
+        if (this.map.getSource('route')) {
+          this.map.removeLayer('route');
+          this.map.removeSource('route');
+        } else {
+          this.map.addLayer({
+            'id': 'route',
+            'type': 'line',
+            'source': {
+              'type': 'geojson',
+              'data': {
+                'type': 'Feature',
+                'properties': {},
+                'geometry': coords
+              }
+            },
+            'layout': {
+              'line-join': 'round',
+              'line-cap': 'round'
+            },
+            'paint': {
+              'line-color': '#03AA46',
+              'line-width': 8,
+              'line-opacity': 0.8
+            }
+          });
+        }
+      }
+      
+      // getInstructions(data) {
+      //   // Target the sidebar to add the instructions
+      //   var directions = document.getElementById('directions');
+      
+      //   var legs = data.legs;
+      //   var tripDirections = [];
+      //   // Output the instructions for each step of each leg in the response object
+      //   for (var i = 0; i < legs.length; i++) {
+      //     var steps = legs[i].steps;
+      //     for (var j = 0; j < steps.length; j++) {
+      //       tripDirections.push('<br><li>' + steps[j].maneuver.instruction) +
+      //       '</li>';
+      //     }
+      //   }
+      //   directions.innerHTML =
+      //   '<br><h2>Trip duration: ' +
+      //   Math.floor(data.duration / 60) +
+      //   ' min.</h2>' +
+      //   tripDirections;
+      // }
+      
+      removeRoute() {
+        if (this.map.getSource('route')) {
+          this.map.removeLayer('route');
+          this.map.removeSource('route');
+        } else {
+          return;
+        }
       }
       
       getIsochrone(km){
@@ -185,7 +370,7 @@ export class HomePage implements OnInit{
             this.coordinates.latitude = data.coords.latitude;
             this.coordinates.longitude = data.coords.longitude;
             
-            var el = document.createElement('div');
+            let el = document.createElement('div');
             el.className = 'bluetooth';
             
             // make a marker for each feature and add to the map
@@ -193,11 +378,56 @@ export class HomePage implements OnInit{
             .setLngLat([this.coordinates.longitude, this.coordinates.latitude])
             .addTo(this.map);
           });
+        }
+        
+        coordinatesGeocoder = (query) => {
+          // Match anything which looks like
+          // decimal degrees coordinate pair.
+          var matches = query.match(
+            /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+            );
+            if (!matches) {
+              return null;
+            }
+            
+            var coord1 = Number(matches[1]);
+            var coord2 = Number(matches[2]);
+            var geocodes = [];
+            
+            if (coord1 < -90 || coord1 > 90) {
+              // must be lng, lat
+              geocodes.push(this.coordinateFeature(coord1, coord2));
+            }
+            
+            if (coord2 < -90 || coord2 > 90) {
+              // must be lat, lng
+              geocodes.push(this.coordinateFeature(coord2, coord1));
+            }
+            
+            if (geocodes.length === 0) {
+              // else could be either lng, lat or lat, lng
+              geocodes.push(this.coordinateFeature(coord1, coord2));
+              geocodes.push(this.coordinateFeature(coord2, coord1));
+            }
+            
+            return geocodes;
+          };
+          
+          
+          coordinateFeature(lng, lat) {
+            return {
+              center: [lng, lat],
+              geometry: {
+                type: 'Point',
+                coordinates: [lng, lat]
+              },
+              place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+              place_type: ['coordinate'],
+              properties: {},
+              type: 'Feature'
+            };
+          }
           
           
         }
         
-        
-        
-      }
-      
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -16,5 +16,15 @@ export class MapService {
     getIsochrone(coordinates:any, minutes:number){
       return this.httpClient.get('https://api.mapbox.com/isochrone/v1/mapbox/cycling/'+coordinates.longitude + '%2C' + coordinates.latitude + '?contours_minutes=' + minutes + '&polygons=true&access_token=' +environment.mapbox.accessToken);
     }
+
+    getMapDraw(coordinates:any, radiuses: any){
+      return this.httpClient.get('https://api.mapbox.com/matching/v5/mapbox/cycling/'+coordinates +'?geometries=geojson&radiuses=' + radiuses + '&steps=true&access_token=' +environment.mapbox.accessToken);
+    }
+
   }
+
+
+  // https://api.mapbox.com/matching/v5/mapbox/cycling/9.000400715348263,39.03559525567064;9.000400715348263,39.03434913471395?geometries=geojson&radiuses=25,25&steps=true&access_token=pk.eyJ1Ijoibmljb2xhbGlzY2kiLCJhIjoiY2tvcTB3YzR1MHFrMzJwc2piZXduMmYxdCJ9.MZquw3FQpejTxMRwJBzutg
+
+  // https://api.mapbox.com/matching/v5/mapbox/driving/9.000382292462064,39.035607089051496;9.000435435875772,39.03432053612866?geometries=geojson&radiuses=25;25&steps=true&access_token=pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2p0MG01MXRqMW45cjQzb2R6b2ptc3J4MSJ9.zA2W0IkI0c6KaAhJfk9bWg
   
